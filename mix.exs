2c8249e297846873901b0e3b8d394787511b276d
Sean Callan <seancallan@gmail.com>
2016-07-10 22:11:12 -0600

    Add Ueberauth and Guardian

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Watercooler.Mixfile do
   def application do
     [mod: {Watercooler, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :ueberauth_facebook]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +30,19 @@ defmodule Watercooler.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0"},
-     {:phoenix_pubsub, "~> 1.0"},
+    [
+     {:cowboy, "~> 1.0"},
+     {:gettext, "~> 0.11"},
+     {:guardian, "~> 0.12.0"},
+     {:phoenix, "~> 1.2.0"},
      {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:phoenix_pubsub, "~> 1.0"},
+     {:postgrex, ">= 0.0.0"},
+     {:ueberauth, "~> 0.2.0"},
+     {:ueberauth_facebook, "~> 0.3.2"}
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
