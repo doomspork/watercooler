84b6878e20377fc9d1a97c03235a3d3936f06180
Sean Callan <seancallan@gmail.com>
2016-07-12 15:48:25 -0600

    Implement User auth

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,15 +1,56 @@
 defmodule Watercooler.AuthController do
   use Watercooler.Web, :controller
 
-  def create(conn, %{"login" => %{"username" => username}}) do
-    conn
-    |> put_resp_cookie("username", username)
-    |> redirect(to: "/lobby")
+  plug Ueberauth
+
+  alias Watercooler.User
+  alias Ueberauth.Strategy.Helpers
+
+  def request(conn, _params) do
+    render(conn, "request.html", callback_url: Helpers.callback_url(conn))
   end
 
   def delete(conn, _params) do
     conn
-    |> delete_resp_cookie("username")
+    |> put_flash(:info, "You have been logged out!")
+    |> configure_session(drop: true)
+    |> redirect(to: "/")
+  end
+
+  def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
+    conn
+    |> put_flash(:error, "Failed to authenticate.")
+    |> redirect(to: "/")
+  end
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    auth
+    |> User.from_auth
+    |> user_response(conn)
+  end
+
+  def user_response(%User{id: nil} = user, conn) do
+    conn
+    |> put_session(:new_user, user)
+    |> redirect(to: "/signup")
+  end
+  def user_response(user, conn) do
+    conn
+    |> Guardian.Plug.sign_in(user)
+    |> redirect(to: "/lobby")
+  end
+
+  def unauthenticated(conn, _params) do
+    conn
+    |> put_status(:forbidden)
+    |> put_flash(:error, "You must be logged in!")
+    |> redirect(to: "/")
+  end
+
+  def unauthorized(conn, _params) do
+    conn
+    |> put_status(:unauthorized)
+    |> put_flash(:error, "You do not have adequate permission!")
     |> redirect(to: "/")
   end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -1,6 +1,8 @@
 defmodule Watercooler.User do
   use Watercooler.Web, :model
 
+  alias Watercooler.Repo
+
   schema "users" do
     field :avatar, :string
     field :username, :string
@@ -17,5 +19,27 @@ defmodule Watercooler.User do
     struct
     |> cast(params, [:avatar, :username, :provider, :provider_uid])
     |> validate_required([:avatar, :username, :provider, :provider_uid])
+  end
+
+  def from_auth(auth) do
+    auth
+    |> lookup_by_auth
+    |> user_from_result
+  end
+
+  defp user_from_result({auth, nil}) do
+    %__MODULE__{avatar: auth.info.image,
+                provider: Atom.to_string(auth.provider),
+                provider_uid: auth.uid}
+  end
+  defp user_from_result({_auth, user}), do: user
+
+  defp lookup_by_auth(%Ueberauth.Auth{provider: provider, uid: uid} = auth) do
+    provider = Atom.to_string(provider)
+    query = from u in __MODULE__,
+            where: u.provider == ^provider and u.provider_uid == ^uid,
+            select: u
+
+    {auth, Repo.one(query)}
   end
 end
